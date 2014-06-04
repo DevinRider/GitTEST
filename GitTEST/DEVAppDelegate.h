1cc008f0fef6edeea8ed470b733bdd0fe0d30f8b
Devin Rider <devinrrider@gmail.com>
2014-06-04 08:05:49 -0700

    Initial Commit

---
+++ GitTEST/DEVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DEVAppDelegate.h
+//  GitTEST
+//
+//  Created by Devin on 6/4/14.
+//  Copyright (c) 2014 Devin Rider. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DEVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitTESTTests/GitTESTTests.m
@@ -0,0 +1,34 @@
+//
+//  GitTESTTests.m
+//  GitTESTTests
+//
+//  Created by Devin on 6/4/14.
+//  Copyright (c) 2014 Devin Rider. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GitTESTTests : XCTestCase
+
+@end
+
+@implementation GitTESTTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
